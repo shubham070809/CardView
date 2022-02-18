599bfee6d7ffde6afd790aef8e25b4dae9cf8daa
shubham070809 <87058175+shubham070809@users.noreply.github.com>
2022-02-18 17:21:29 +0530

    Add files via upload

---
+++ JavaPractice/src/com/company/Dog.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Dog {
+
+    private int weight;
+    public int num=01;
+    public int getWeight()
+    {
+        return weight;
+    }
+    public void setWeight(int w)
+    {
+        if(w>0)
+        {
+            weight=w;
+        }
+    }
+
+}
---
+++ JavaPractice/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Dog d=new Dog();
+        d.setWeight(508);
+//        int w=d.getWeight();
+        System.out.println(d.getWeight() + " "+d.num);
+    }
+}
